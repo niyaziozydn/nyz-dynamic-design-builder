9f0d76d194c50f30607901b81ab713c8c854dd93
niyazi <niyazimuhammed.ozaydin@baykartech.com>
2025-07-15 11:21:32 +0300

    Update function signatures to include previous and current data values; version bump to 2.0.1

--- nyz_dynamic_design_builder/builder.py
+++ nyz_dynamic_design_builder/builder.py
@@ -20,7 +20,8 @@ __all__ = [
 ]
 
 
-def search_fk_value(obj, field, search_fk_list):
+
+def search_fk_value(obj, field, search_fk_list,previous_data_value=None,current_data_value=None):
     for search_field in search_fk_list:
         try:
             value = getattr(obj, field)
@@ -28,13 +29,16 @@ def search_fk_value(obj, field, search_fk_list):
             if isinstance(field, (DateField, DateTimeField)):
                 value = value.strftime("%Y-%m-%d %H:%M:%S") if value else ""
             if value:
+                if previous_data_value and current_data_value and isinstance(value, str) and previous_data_value in value:
+                    value = current_data_value
                 return value
+
         except AttributeError:
             return ""
     return ""
 
 
-def get_special_m2m_fields(m2m_obj, sub_fields, search_fk_list=None):
+def get_special_m2m_fields(m2m_obj, sub_fields, search_fk_list=None,previous_data_value=None,current_data_value=None):
     rows = {}
     if not m2m_obj:
         return ""
@@ -43,7 +47,7 @@ def get_special_m2m_fields(m2m_obj, sub_fields, search_fk_list=None):
         try:
             value = getattr(m2m_obj, sub_field, "")
             if isinstance(value, ForeignKey):
-                value = search_fk_value(m2m_obj, sub_field, search_fk_list)
+                value = search_fk_value(m2m_obj, sub_field, search_fk_list,previous_data_value,current_data_value)
             if callable(value):
                 value = value()
             verbose_name = m2m_obj._meta.get_field(sub_field).verbose_name
@@ -55,12 +59,12 @@ def get_special_m2m_fields(m2m_obj, sub_fields, search_fk_list=None):
     return rows
 
 
-def get_m2m_fields(field_name, m2m_values, search_fk_list, ignore_m2m_fields=None, special_m2m_fields=None, current_field=None):
+def get_m2m_fields(field_name, m2m_values, search_fk_list, ignore_m2m_fields=None, special_m2m_fields=None, current_field=None,previous_data_value=None,current_data_value=None):
     m2m_rows = {}
     for index, m2m_value in enumerate(m2m_values, start=1):
         for field in m2m_value._meta.fields:
             if special_m2m_fields and current_field in special_m2m_fields:
-                special_dict = get_special_m2m_fields(m2m_value, special_m2m_fields[current_field], search_fk_list)
+                special_dict = get_special_m2m_fields(m2m_value, special_m2m_fields[current_field], search_fk_list,previous_data_value,current_data_value)
                 m2m_rows.update(special_dict)
                 continue
             if ignore_m2m_fields and field.name in ignore_m2m_fields:
@@ -70,13 +74,13 @@ def get_m2m_fields(field_name, m2m_values, search_fk_list, ignore_m2m_fields=Non
             if isinstance(field, (DateField, DateTimeField)):
                 value = value.strftime("%Y-%m-%d %H:%M:%S") if value else ""
             if isinstance(field, ForeignKey):
-                value = search_fk_value(m2m_value, field.name, search_fk_list)
+                value = search_fk_value(m2m_value, field.name, search_fk_list,previous_data_value,current_data_value)
             col_name = f"{field_name} {index} - {verbose}"
             m2m_rows[col_name] = value
     return m2m_rows
 
 
-def get_special_fk_fields(obj, field_name, sub_fields, search_fk_list=None):
+def get_special_fk_fields(obj, field_name, sub_fields, search_fk_list=None,previous_data_value=None,current_data_value=None):
     rows = {}
     fk_obj = getattr(obj, field_name, None)
     if not fk_obj:
@@ -86,7 +90,7 @@ def get_special_fk_fields(obj, field_name, sub_fields, search_fk_list=None):
         try:
             value = getattr(fk_obj, sub_field, "")
             if isinstance(value, ForeignKey):
-                value = search_fk_value(fk_obj, sub_field, search_fk_list)
+                value = search_fk_value(fk_obj, sub_field, search_fk_list,previous_data_value,current_data_value)
             if callable(value):
                 value = value()
             verbose_name = fk_obj._meta.get_field(sub_field).verbose_name
@@ -99,7 +103,7 @@ def get_special_fk_fields(obj, field_name, sub_fields, search_fk_list=None):
 
 
 def export_data(field_list=None, query=None, search_fk_list=None, ignore_m2m_fields=None,
-                special_fk_values=None, special_m2m_fields=None):
+                special_fk_values=None, special_m2m_fields=None,previous_data_value=None,current_data_value=None):
     """
     Export a Django queryset to a pandas DataFrame with intelligent handling of
     ForeignKey, ManyToMany, and date fields.
@@ -125,25 +129,33 @@ def export_data(field_list=None, query=None, search_fk_list=None, ignore_m2m_fie
             try:
                 field_obj = model._meta.get_field(field)
                 verbose_name = field_obj.verbose_name
+                if getattr(field_obj, "choices", None): # for choices field *
+                    display_method = getattr(obj, f"get_{field}_display", None)
+                    if callable(display_method):
+                        row[verbose_name] = display_method()
+                        continue
                 value = getattr(obj, field)
 
                 if isinstance(field_obj, (DateField, DateTimeField)):
                     value = value.strftime("%Y-%m-%d" if isinstance(field_obj, DateField) else "%Y-%m-%d %H:%M:%S") if value else ""
 
+
+
+
                 if isinstance(field_obj, ForeignKey):
                     if special_fk_values and field in special_fk_values:
                         sub_fields = special_fk_values[field]
-                        special_dict = get_special_fk_fields(obj, field, sub_fields, search_fk_list)
+                        special_dict = get_special_fk_fields(obj, field, sub_fields, search_fk_list,previous_data_value,current_data_value)
                         row.update(special_dict)
                         continue
                     else:
-                        value = search_fk_value(obj, field, search_fk_list)
+                        value = search_fk_value(obj, field, search_fk_list,previous_data_value, current_data_value)
                         row[verbose_name] = value
 
                 elif isinstance(field_obj, ManyToManyField):
                     m2m_values = value.all()
                     m2m_dict = get_m2m_fields(verbose_name, m2m_values, search_fk_list, ignore_m2m_fields,
-                                              special_m2m_fields, field)
+                                              special_m2m_fields, field,previous_data_value, current_data_value)
                     row.update(m2m_dict)
 
                 else:
